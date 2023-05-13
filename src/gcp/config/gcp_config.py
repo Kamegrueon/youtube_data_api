32cbdac7d775091fc2d95a712d8c40f1a47fb62c
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2023-05-13 09:44:45 +0900

    Fix: シークレットJSONのパスを環境変数から読み取れるように修正

--- src/gcp/config/gcp_config.py
+++ src/gcp/config/gcp_config.py
@@ -1,1 +1,10 @@
-PROJECT_ID = "youtube_data_api"
+import os
+
+from dotenv import load_dotenv
+
+# dotenv_path = "/Users/kengo/projects/youtube_data_api/.env"
+load_dotenv()
+
+GOOGLE_KEY_PATH = os.environ["KEY_PATH"]
+
+PROJECT_ID = "youtube-data-api-385206"
