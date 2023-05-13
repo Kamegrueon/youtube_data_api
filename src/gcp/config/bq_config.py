598b2b5d45599a0a3ab5d08bec0b1afe240a3c99
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2023-05-13 09:46:23 +0900

    Fix: cloud上のbigqueryに接続できるように修正

--- src/gcp/config/bq_config.py
+++ src/gcp/config/bq_config.py
@@ -20,7 +20,7 @@ MOST_POPULAR_TABLE_SCHEMA = [
         'CREATED_AT', 'DATETIME', mode='REQUIRED', description='データ取得日'
     ),
     bigquery.SchemaField(
-        'CATEGORY_ID', 'STRING', mode='REQUIRED', description='カテゴリID'
+        'CATEGORY_ID', 'INTEGER', mode='REQUIRED', description='カテゴリID'
     ),
     bigquery.SchemaField(
         'DURATION', 'STRING', mode='REQUIRED', description='動画の長さ'
@@ -29,22 +29,18 @@ MOST_POPULAR_TABLE_SCHEMA = [
         'VIEW_COUNT', 'INTEGER', mode='NULLABLE', description='視聴回数'
     ),
     bigquery.SchemaField(
-        'LIKE_COUNT', 'FLOAT', mode='NULLABLE', description='高評価数'
+        'LIKE_COUNT', 'INTEGER', mode='NULLABLE', description='高評価数'
     ),
     bigquery.SchemaField(
-        'DISLIKE_COUNT', 'FLOAT', mode='NULLABLE', description='低評価数'
+        'DISLIKE_COUNT', 'INTEGER', mode='NULLABLE', description='低評価数'
     ),
     bigquery.SchemaField(
-        'FAVORITE_COUNT', 'STRING', mode='NULLABLE', description='お気に入り数'
+        'FAVORITE_COUNT', 'INTEGER', mode='NULLABLE', description='お気に入り数'
     ),
     bigquery.SchemaField(
-        'COMMENT_COUNT', 'STRING', mode='NULLABLE', description='コメント数'
+        'COMMENT_COUNT', 'INTEGER', mode='NULLABLE', description='コメント数'
     ),
     bigquery.SchemaField(
-        'TAGS', 'RECORD', mode='REPEATED', description='タグ情報',
-        fields=[
-            bigquery.SchemaField(
-                'TAG_NAME', 'STRING', mode='NULLABLE', description='タグ名称'
-            ),
-        ])
+        'TAGS', 'STRING', mode='REPEATED', description='タグ情報',
+    )
 ]
