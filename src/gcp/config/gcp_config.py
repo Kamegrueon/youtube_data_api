f16e86eed3c009890edc142f39502c78be175e65
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2023-05-13 14:38:32 +0900

    Fix: 本物のGCSに接続する処理に変更

--- src/gcp/config/gcp_config.py
+++ src/gcp/config/gcp_config.py
@@ -2,9 +2,9 @@ import os
 
 from dotenv import load_dotenv
 
-# dotenv_path = "/Users/kengo/projects/youtube_data_api/.env"
 load_dotenv()
 
-GOOGLE_KEY_PATH = os.environ["KEY_PATH"]
+GOOGLE_KEY_PATH = os.environ["GCP_KEY_PATH"]
 
 PROJECT_ID = "youtube-data-api-385206"
+BUCKET_NAME = "youtube-data-api-bucket4346542"
--- src/gcp/gcs.py
+++ src/gcp/gcs.py
@@ -3,21 +3,24 @@ import os
 import shutil
 from typing import Optional
 
-from google.auth.credentials import AnonymousCredentials
+# from google.auth.credentials import AnonymousCredentials
 from google.cloud.storage import Client
 from google.cloud.storage.blob import Blob
+from google.oauth2.service_account import Credentials
 
-from gcp.config.gcp_config import PROJECT_ID
+from gcp.config.gcp_config import GOOGLE_KEY_PATH, PROJECT_ID
 
 # GCSのオブジェクトを取得する
 # curl http://host.docker.internal:4443/storage/v1/b/youtube/o
 
 
 class GCS:
-    def __init__(self, bucket_name: str) -> None:
+    def __init__(self, bucket_name: str, project_id: str = PROJECT_ID) -> None:
+        credentials = Credentials.from_service_account_file(GOOGLE_KEY_PATH)
         self.client = Client(
-            credentials=AnonymousCredentials(),
-            project=PROJECT_ID,
+            # credentials=AnonymousCredentials(),
+            credentials=credentials,
+            project=project_id,
         )
         self.bucket_name = bucket_name
 
@@ -28,6 +31,9 @@ class GCS:
         local_full_path = f"/app/tmp/cloud-storage/data/{blob.name}"
         os.remove(local_full_path)
 
+    def create_bucket(self) -> None:
+        self.client.create_bucket(self.bucket_name, location="us")
+
     def upload_file(self, gcs_path: str, local_path: str) -> None:
         # local_path = "src/json/json_file.json"
         bucket = self.client.get_bucket(self.bucket_name)
@@ -35,16 +41,18 @@ class GCS:
 
         blob_gcs.upload_from_filename(local_path)
 
-        gcp_full_path = f"/app/tmp/cloud-storage/data/{gcs_path}"
-        self.__local_copy_process(local_path, gcp_full_path)
+        # エミュレーター用
+        # gcp_full_path = f"/app/tmp/cloud-storage/data/{gcs_path}"
+        # self.__local_copy_process(local_path, gcp_full_path)
 
     def download_file(self, gcs_path: str, local_path: str) -> None:
         bucket = self.client.get_bucket(self.bucket_name)
         blob = bucket.blob(gcs_path)
         blob.download_to_filename(local_path)
 
-        gcp_full_path = f"/app/tmp/cloud-storage/data/{gcs_path}"
-        self.__local_copy_process(gcp_full_path, local_path)
+        # エミュレーター用
+        # gcp_full_path = f"/app/tmp/cloud-storage/data/{gcs_path}"
+        # self.__local_copy_process(gcp_full_path, local_path)
 
     def get_file(self, blob_path: str) -> Optional[Blob]:
         bucket = self.client.bucket(self.bucket_name)
@@ -53,7 +61,9 @@ class GCS:
 
     def delete_file(self, blob: Blob) -> None:
         blob.delete()
-        self.__local_delete_process(blob)
+
+        # エミュレーター用
+        # self.__local_delete_process(blob)
 
     def delete_all_files(self) -> None:
         bucket = self.client.bucket(self.bucket_name)
@@ -62,23 +72,25 @@ class GCS:
 
 
 if __name__ == "__main__":
-    # from api.youtube_api import YoutubeApiRequest
-    BUCKET_NAME = "youtube"
+    from api.youtube_api import YoutubeApiRequest
+    BUCKET_NAME = PROJECT_ID
 
-    # youtube = YoutubeApiRequest()
+    youtube = YoutubeApiRequest()
     gcs = GCS(bucket_name=BUCKET_NAME)
+    # gcs.create_bucket()
 
-    # youtube.get_youtube_data_to_json()
-    # LOCAL_FILE_PATH =
-    # f"{youtube.output_path}/{youtube.date_str}_popular.json"
+    youtube.get_youtube_data_to_json()
+
+    LOCAL_FILE_PATH = f"{youtube.output_path}/{youtube.date_str}_popular.json"
     # GCS_FILE_PATH = f"{gcs.bucket_name}/{youtube.date_str}_popular.json"
+    GCS_FILE_PATH = f"most_popular/{youtube.date_str}_popular.json"
 
-    gcs.delete_all_files()
+    # gcs.delete_all_files()
 
-    # gcs.upload_file(
-    #     gcs_path=GCS_FILE_PATH,
-    #     local_path=LOCAL_FILE_PATH
-    # )
+    gcs.upload_file(
+        gcs_path=GCS_FILE_PATH,
+        local_path=LOCAL_FILE_PATH
+    )
 
     # blob = gcs.get_file(GCS_FILE_PATH)
     # if blob is not None:
