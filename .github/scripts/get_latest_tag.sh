48ada9714d803b5bee446eff027d87127948bd13
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2024-06-10 14:07:13 +0900

    fix

--- .github/scripts/get_latest_tag.sh
+++ .github/scripts/get_latest_tag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LATEST_TAG=$(gcloud artifacts docker images list --sort-by=~CREATE_TIME --include-tags --format='value(TAGS)' --filter="tags:*" --limit=1 "${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPOSITORY_NAME}/${ENV_STRING}-api-image" || echo "default")
+LATEST_TAG=$(gcloud artifacts docker images list --sort-by=~CREATE_TIME --include-tags --format='value(TAGS)' --filter="tags:*" --limit=1 "$REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY_NAME/$ENV_STRING-api-image" || echo "default")
 if [ "$LATEST_TAG" == "default" ]; then
   echo "Failed to get the latest tag, Use the tag default."
 else
