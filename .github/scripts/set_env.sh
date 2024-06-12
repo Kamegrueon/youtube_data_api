a3f611e29a0e8031003257d9836dc08ceeeee410
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2024-06-12 09:07:40 +0900

    fix: outputs repository name

--- .github/scripts/set_env.sh
+++ .github/scripts/set_env.sh
@@ -2,8 +2,6 @@
 
 if [[ "$GITHUB_REF" == "refs/heads/main" || "$GITHUB_BASE_REF" == "main" ]]; then
   echo "ENV_STRING=prd" >> "$GITHUB_OUTPUT"
-  echo "REPOSITORY_NAME=prd-$BASE_REPOSITORY_NAME" >> "$GITHUB_OUTPUT"
 elif [[ "$GITHUB_REF" == "refs/heads/stg" || "$GITHUB_BASE_REF" == "stg" ]]; then
   echo "ENV_STRING=stg" >> "$GITHUB_OUTPUT"
-  echo "REPOSITORY_NAME=stg-$BASE_REPOSITORY_NAME" >> "$GITHUB_OUTPUT"
 fi
