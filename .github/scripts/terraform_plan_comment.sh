16950b7ae698aa90efa60ea1f877bf62389b3edd
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2024-06-13 09:49:18 +0900

    test

--- .github/scripts/terraform_plan_comment.sh
+++ .github/scripts/terraform_plan_comment.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-lf="\n"
+#!/bin/bash
+
+lf="%0A"
+message="#### Terraform Format and Style 🖌${FMT_OUTCOME}"
+message+="${lf}#### Terraform Initialization ⚙️${INIT_OUTCOME}"
+message+="${lf}#### Terraform Validation 🤖${VALIDATE_OUTCOME}"
+message+="${lf}<details><summary>Validation Output</summary>"
+message+="${lf}\`\`\`"
+message+="${lf}${VALIDATE_OUTPUT}"
+message+="${lf}\`\`\`"
+message+="${lf}</details>"
+message+="${lf}#### Terraform Plan 📖${PLAN_OUTCOME}"
+message+="${lf}<details><summary>Show Plan</summary>"
+message+="${lf}\`\`\`"
+message+="${lf}terraform\n${PLAN_OUTPUT}"
+message+="${lf}\`\`\`"
+message+="${lf}</details>"
+message+="${lf}*Pusher: @${GITHUB_ACTOR}, Action: ${GITHUB_EVENT_NAME}, Working Directory: ${GITHUB_WORKING_DIR}, Workflow: ${GITHUB_WORKFLOW}*"
+echo "message=${message}" >> $GITHUB_OUTPUT
 message="#### Terraform Format and Style 🖌${FMT_OUTCOME}"
 message+="${lf}#### Terraform Initialization ⚙️${INIT_OUTCOME}"
 message+="${lf}#### Terraform Validation 🤖${VALIDATE_OUTCOME}"
