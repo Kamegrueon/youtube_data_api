18e267e7797b62ac26df4404d1bdcd4ea526c7b2
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2024-06-15 15:05:41 +0900

    fix: マークダウンの修正

--- .github/scripts/terraform_plan_comment.sh
+++ .github/scripts/terraform_plan_comment.sh
@@ -13,11 +13,11 @@ message+="\`\`\`${lf}"
 message+="</details>${lf}${lf}"
 message+="#### Terraform Plan 📖${PLAN_OUTCOME}${lf}"
 message+="<details><summary>Show Plan</summary>${lf}${lf}"
-message+="```${lf}"
+message+="\`\`\`${lf}"
 message+="${PLAN_OUTPUT}${lf}"
-message+="```${lf}"
+message+="\`\`\`${lf}"
 message+="</details>${lf}${lf}"
 message+="**Pusher: @${GITHUB_ACTOR}, Action: ${GITHUB_EVENT_NAME}, Working Directory: ${GITHUB_WORKING_DIR}, Workflow: ${GITHUB_WORKFLOW}**"
 
 # メッセージの出力
-echo -e $message > terraform_comment.txt
+echo -e "$message" > terraform_comment.txt
