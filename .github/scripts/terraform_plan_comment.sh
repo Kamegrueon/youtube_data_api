3212faacb56858b0b588aff6e24d721901d999fa
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2024-06-15 13:35:24 +0900

    test

--- .github/scripts/terraform_plan_comment.sh
+++ .github/scripts/terraform_plan_comment.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-lf="\n"
-message="#### Terraform Format and Style 🖌${FMT_OUTCOME}"
-message+="${lf}#### Terraform Initialization ⚙️${INIT_OUTCOME}"
-message+="${lf}#### Terraform Validation 🤖${VALIDATE_OUTCOME}"
-message+="${lf}<details><summary>Validation Output</summary>"
-message+="${lf}\`\`\`${lf}${VALIDATE_OUTPUT}${lf}\`\`\`${lf}</details>"
-message+="${lf}#### Terraform Plan 📖${PLAN_OUTCOME}"
-message+="${lf}<details><summary>Show Plan</summary>"
-message+="${lf}\`\`\`${lf}terraform\n${PLAN_OUTPUT}${lf}\`\`\`${lf}</details>"
-message+="${lf}*Pusher: @${GITHUB_ACTOR}, Action: ${GITHUB_EVENT_NAME}, Working Directory: ${GITHUB_WORKING_DIR}, Workflow: ${GITHUB_WORKFLOW}*"
-echo "message=${message}" >> $GITHUB_OUTPUT
+lf="%0A"  # GitHub Actionsの改行エスケープシーケンス
+
+# メッセージの構築
+message="#### Terraform Format and Style 🖌${FMT_OUTCOME}${lf}"
+message+="#### Terraform Initialization ⚙️${INIT_OUTCOME}${lf}"
+message+="#### Terraform Validation 🤖${VALIDATE_OUTCOME}${lf}"
+message+="<details><summary>Validation Output</summary>${lf}\`\`\`${lf}${VALIDATE_OUTPUT}${lf}\`\`\`${lf}</details>${lf}"
+message+="#### Terraform Plan 📖${PLAN_OUTCOME}${lf}"
+message+="<details><summary>Show Plan</summary>${lf}\`\`\`terraform${lf}${PLAN_OUTPUT}${lf}\`\`\`${lf}</details>${lf}"
+message+="*Pusher: @${GITHUB_ACTOR}, Action: ${GITHUB_EVENT_NAME}, Working Directory: ${GITHUB_WORKING_DIR}, Workflow: ${GITHUB_WORKFLOW}*"
+
+# メッセージの出力
+echo "message=$message" >> $GITHUB_OUTPUT
