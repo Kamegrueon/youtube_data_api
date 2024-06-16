f5b7e6c5c087ab95e10f98ea41402c1cb7436ae1
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2024-06-16 15:23:22 +0900

    fix: dev workflow追加

---
+++ .github/scripts/set-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ "$GITHUB_REF" == "refs/heads/main" || "$GITHUB_BASE_REF" == "main" ]]; then
+  echo "ENV_STRING=prd" >> "$GITHUB_OUTPUT"
+elif [[ "$GITHUB_REF" == "refs/heads/stg" || "$GITHUB_BASE_REF" == "stg" ]]; then
+  echo "ENV_STRING=stg" >> "$GITHUB_OUTPUT"
+else
+    echo "ENV_STRING=dev" >> "$GITHUB_OUTPUT"
+fi
