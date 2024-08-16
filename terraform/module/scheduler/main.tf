f081efd407c4beb41fe1bd7b963aaa070645b562
AnagumaReonKK <niwaniwaniwaniwatori.gairuze@gmail.com>
2024-08-16 16:38:32 +0900

    change: post params value by scheduler

--- terraform/module/scheduler/main.tf
+++ terraform/module/scheduler/main.tf
@@ -1,4 +1,17 @@
+locals {
+  params = [
+    {
+      url_path : "/invoke/transfer",
+      prefix : "most_popular",
+      part : "snippet, contentDetails, statistics",
+      chart : "mostPopular",
+      maxResults : 50
+    }
+  ]
+}
+
 resource "google_cloud_scheduler_job" "invoke-transfer" {
+  for_each    = { for i in local.params : i.url_path => i }
   paused      = terraform.workspace == "prd" ? false : true
   name        = "${terraform.workspace}-invoke-transfer"
   project     = var.gcp_project_id
@@ -11,12 +24,17 @@ resource "google_cloud_scheduler_job" "invoke-transfer" {
   }
 
   http_target {
+
     http_method = "POST"
-    uri         = "${var.cloud_run_api_uri}${var.invoke_transfer_url_path}"
+    uri         = "${var.cloud_run_api_uri}${each.value.url_path}"
+    body        = base64encode("{\"prefix\":\"${each.value.prefix}\",\"part\":\"${each.value.part}\",\"chart\":\"${each.value.chart}\",\"maxResults\":${each.value.maxResults}}")
+    headers = {
+      "Content-Type" = "application/json"
+    }
 
     oidc_token {
       service_account_email = var.service_account_invoker_email
-      audience              = "${var.cloud_run_api_uri}${var.invoke_transfer_url_path}"
+      audience              = "${var.cloud_run_api_uri}${each.value.url_path}"
     }
   }
 
